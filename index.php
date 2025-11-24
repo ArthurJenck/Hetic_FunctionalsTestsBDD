48e960edf47537e26b1e6ac17d3a37e2ad3cb251
Arthur Jenck <arthurjenckdev@gmail.com>
2025-11-24 16:25:29 +0100

    feat: style

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <div class="background"></div>
     <form action="./signup.php" method="POST">
         <h1>Formulaire d'inscription</h1>
-        <label for="email">Adresse mail*</label>
-        <input type="email" id="email" name="email" required>
+        <div>
+            <label for="email">Adresse mail*</label>
+            <input type="email" id="email" name="email" required autocomplete="email">
+        </div>
 
-        <label for="password">Mot de passe*</label>
-        <input type="password" id="password" name="password" required>
+        <div>
+            <label for="password">Mot de passe*</label>
+            <input type="password" id="password" name="password" required autocomplete="password">
+        </div>
 
         <button type="submit">Envoyer</button>
     </form>
